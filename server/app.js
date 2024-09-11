b8c3e7a8b83bd6573a6116dadd044a861058f16c
warrenmblood <blood.warren@gmail.com>
2024-09-11 18:31:33 -0400

    express server

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+import { generatePlan } from "../src/utils.js";
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -7,3 +9,48 @@ app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 
 app.use(express.static("build"));
 app.use(bodyParser.json());
+
+const jargon = {
+    easy: {
+      term: "easy pace",
+      def: 'a relaxed, "conversational" pace. Under 75% Max HR'
+    },
+    tempo: {
+      term: "tempo pace",
+      def: "a harder effort, but maintainable for 40-60 minutes. 85-90% Max HR"
+    }
+};
+
+const offDay = {
+    title: "Off Day",
+    description: "Take a day to rest.",
+    terms: []
+};
+
+const today = new Date();
+
+const sampleInputs = {
+    goalDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 90),
+    startDate: today,
+    goalDistance: 26.2,
+    name: "Boston Marathon 2025",
+    runsPerWeek: 5,
+    startDistance: 9,
+    unit: "mi"
+};
+
+const samplePlan = generatePlan(sampleInputs, offDay, jargon);
+const plans = [samplePlan];
+
+app.get("/api/plans", (req, res) => {
+    console.log("GET successful");
+    res.send(plans);
+});
+
+app.put("/api/plans/put", (req, res) => {
+    console.log("PUT successful");
+    let newPlan = req.body;
+    plans.push(newPlan);
+    res.send(plans);
+});
+
---
+++ server/build/precache-manifest.5eee72f5c4add2de8308db752d50f320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8b17520942352bc5c7982dc67934bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25db6f06d23351f6de31",
+    "url": "/static/css/main.fd040d34.chunk.css"
+  },
+  {
+    "revision": "802d4ce7cc7d8bb5f596",
+    "url": "/static/js/2.24b1e479.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "25db6f06d23351f6de31",
+    "url": "/static/js/main.46230e0b.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  }
+]);
